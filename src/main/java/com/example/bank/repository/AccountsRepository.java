a0709b70cdf6b7f644c328a4d5b042442f9e3027
AbhishekDhaneshwar <dhaneshwarabhishek@gmail.com>
2026-07-05 20:31:52 +0530

    added repository

---
+++ src/main/java/com/example/bank/repository/AccountsRepository.java
@@ -0,0 +1,19 @@
+package com.example.bank.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import com.example.bank.entity.Account;
+import jakarta.transaction.Transactional;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
+
+}
