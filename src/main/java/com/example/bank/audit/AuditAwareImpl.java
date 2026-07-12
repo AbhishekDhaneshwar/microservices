1181e49dd12b0476b7e7aefa3a925be80bf83ced
AbhishekDhaneshwar <dhaneshwarabhishek@gmail.com>
2026-07-13 00:11:25 +0530

    updated AuditImpl

--- src/main/java/com/example/bank/audit/AuditAwareImpl.java
+++ src/main/java/com/example/bank/audit/AuditAwareImpl.java
@@ -2,7 +2,9 @@ package com.example.bank.audit;
 
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
+@Component("auditAwareImpl")
 public class AuditAwareImpl implements AuditorAware<String> {
 
     @Override
